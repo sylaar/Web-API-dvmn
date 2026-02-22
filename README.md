a398525ad468d0c04fc84abd780fda8637d8bc93
Ладыгин Никита <olesya_lakunina@MacBook-Air-de-Olesa.local>
2026-02-22 20:05:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Обрезка ссылок с помощью VK
+
+Скрипт для создания короткой ссылки или получения статистики по уже созданной короткой ссылке.
+
+### Как установить
+
+Создать приложение:
+[dev.vk.com](https://dev.vk.com/ru/admin/created-apps-list?nocode=true)
+
+Перейти в настркойки приложения:
+[dev.vk.com](https://dev.vk.com/ru/admin/app-settings/{YOUR_APP_ID}/info)
+
+В меню "Разработка" выбрать "Ключи доступа"
+[dev.vk.com](https://dev.vk.com/ru/admin/app-settings/{YOUR_APP_ID}/info/keys)
+
+Скопировать "Сервисный ключ" - это и есть VK_TOKEN, используемый в данном скрипте.
+
+Python3 должен быть уже установлен. 
+Затем используйте `pip` (или `pip3`, есть конфликт с Python2) для установки зависимостей:
+```
+pip install -r requirements.txt
+```
+
+### Цель проекта
+
+Код написан в образовательных целях на онлайн-курсе для веб-разработчиков [dvmn.org](https://dvmn.org/).
+
+### Пример запуска
+
+```
+git clone https://github.com/sylaar/dvmn.git
+cd lesson_2
+pip install -r requirements.txt
+```
+`python3 script.py https://dvmn.org/`
---
+++ lesson_2/script.py
@@ -0,0 +1,90 @@
+import argparse
+import os
+import requests
+
+from urllib.parse import urlparse
+
+from dotenv import load_dotenv
+
+
+def shorten_link(token, url):
+    url_api_method_vk = 'https://api.vk.ru/method/utils.getShortLink'
+    params = {
+        'v': '5.199',
+    }
+    payload = {
+        'url': url,
+    }
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    response = requests.post(url_api_method_vk,
+                             params=params,
+                             data=payload,
+                             headers=headers)
+    response.raise_for_status()
+    response_data = response.json()
+    if 'error' in response_data:
+        raise ValueError(response_data['error']['error_msg'])
+    return response_data['response']['short_url']
+
+
+def count_clicks(token, link):
+    shortened_link = urlparse(link).path[1:]
+    url_api_method_vk = 'https://api.vk.ru/method/utils.getLinkStats'
+    params = {
+        'v': '5.199',
+    }
+    payload = {
+        'key': shortened_link,
+    }
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    response = requests.post(url_api_method_vk,
+                             params=params,
+                             data=payload,
+                             headers=headers)
+    response.raise_for_status()
+    response_data = response.json()
+    if 'error' in response_data :
+        raise ValueError(response_data['error']['error_msg'])
+    if not response_data['response']['stats']:
+        return 0
+    return response_data['response']['stats'][0]['views']
+
+
+def is_shorten_link(token, url):
+    try:
+        count_clicks(token, url)
+    except ValueError:
+        return False
+    return True
+    
+
+def main():
+    load_dotenv()
+    vk_token = os.environ['VK_TOKEN']
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', help='Ссылка для сокращения или сокращенная ссылка')
+    args = parser.parse_args()
+
+    if is_shorten_link(vk_token, args.url):
+        try:
+            print(f'Количество переходов: {count_clicks(vk_token, args.url)}')
+        except ValueError as error:
+            print(f'Ошибка: {error}')
+        except requests.exceptions.HTTPError as error:
+            print(f'Ошибка: {error}')
+    else:    
+        try:
+            print(f'Сокращенная ссылка: {shorten_link(vk_token, args.url)}')
+        except ValueError as error:
+            print(f'Ошибка: {error}')
+        except requests.exceptions.HTTPError as error:
+            print(f'Ошибка: {error}')
+
+if __name__ == '__main__':
+    main()
